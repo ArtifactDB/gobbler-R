5dd1a289ac567a106aafea525e8854ac5281f131
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-27 13:36:32 -0700

    Restrict number of attempts to allocate an upload directory.
    Now it fails with a useful error message instead of just looping forever.

--- R/allocateUploadDirectory.R
+++ R/allocateUploadDirectory.R
@@ -3,6 +3,7 @@
 #' Create a new subdirectory in the staging directory to store files for an upload via \code{\link{uploadDirectory}}.
 #'
 #' @param staging String containing the path to the staging directory.
+#' @param attempts Integer scalar specifying the number of attempts to allocate a new directory, after which an error is thrown.
 #'
 #' @return String containing a path to a newly created subdirectory within \code{staging}.
 #'
@@ -14,13 +15,13 @@
 #' allocateUploadDirectory(staging)
 #' 
 #' @export
-allocateUploadDirectory <- function(staging) {
+allocateUploadDirectory <- function(staging, attempts = 3) {
     sinfo <- Sys.info()
     usernames <- unlist(sinfo[c("user", "login", "effective_user")])
 
     # Ensure that we create a unique path inside the staging
     # directory that is actually owned by the current user.
-    while (TRUE) {
+    for (i in seq_len(attempts)) {
         tmp <- tempfile(tmpdir=staging, pattern="upload-")
         dir.create(tmp, showWarnings=FALSE)
         if (file.info(tmp)$uname %in% usernames) {
